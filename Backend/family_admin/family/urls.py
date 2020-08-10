0190555b8fa3865f24588cfcb3c500e0dfd588a9
Leonardo Mendoza <leomk1998@hotmail.com>
2020-08-10 01:41:40 -0500

    Cambios en urls, asgi y venv

--- Backend/family_admin/family/urls.py
+++ Backend/family_admin/family/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
+     path('', signup),
      path('index', index),
      path('signup', signup, name='signup'),
      path('logout', logout, name='logout'),
